73cebea599204fde75bbd10d238eee02985f5b6e
Eimante Uselyte <euselyt@corp.transunion.com>
2022-04-27 18:12:41 +0300

    Add project files.

---
+++ FirstLesson.Tests/CalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions; 
+
+
+namespace FirstLesson.Tests
+{
+   [TestFixture]
+    public class CalculatorTests
+    {
+       [TestCase(1, 6, 7)]
+       [TestCase(2, 2, 4)]
+       [TestCase(5, 1, 6)]
+        public void Add_ReturnsSum(double x, double y, double expected)
+       {
+            Calculator calculator = new Calculator();
+            
+            double actual = calculator.Add(x, y);
+
+            actual.Should().Be(expected);
+       }
+
+    }
+
+}
---
+++ FirstLesson/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstLesson
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Enter a number");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double sum;
+            sum = calculator.Add(x, y);
+            Console.WriteLine("The sum of two numbers is:");
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
